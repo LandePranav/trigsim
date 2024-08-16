64cf85184ddd92db350ccdcc0019f5e10b47fb56
Zenitsu-god <pranavlande1@gmail.com>
2024-08-16 17:30:21 +0530

    svg dragging done

--- src/components/DraggableResizableShape.jsx
+++ src/components/DraggableResizableShape.jsx
@@ -7,35 +7,44 @@ import Navbar from "./Navbar";
 import Target from "../assets/Target" ;
 
 export default function DraggableResizableShape() {
-    const [position, setPosition] = useState({x:50, y:window.innerWidth/5 +200}) ;
-    const [height, setHeight] = useState(300) ;
-    const [isDragging, setIsDragging] = useState(false) ;
-    const [isResizing, setIsResizing] = useState(false) ;
+    const [CameramanPosition, setCamermanPosition] = useState({x:50, y:-100}) ;
+    // const [CameramanHeight, setCameramanHeight] = useState(300) ;
+    const [isDraggingCameraman, setIsDraggingCameraman] = useState(false) ;
 
-    const handlePointerDownDrag = (e) => {
-        setIsDragging(true) ;
-        e.stopPropagation() ;
-    }
+    const [targetPosition, setTargetPosition] = useState({x:window.innerWidth - 200, y:-300});
+    const [isDraggingTarget, setIsDraggingTarget] = useState(false) ;
 
-    const handlePointerDownResize = (e) => {
-        setIsResizing(true) ;
-        e.stopPropagation();
-    }
+    // const handlePointerDownDrag = (e) => {
+    //     setIsDragging(true) ;
+    //     e.stopPropagation() ;
+    // }
+
+    // const handlePointerDownResize = (e) => {
+    //     setIsResizing(true) ;
+    //     e.stopPropagation();
+    // }
 
     const handlePointerMove = (e) => {
-        if(isDragging){
-            setPosition({x:e.clientX-50, y:e.clientY-height/2}) ;
-        } else if (isResizing){
-            const newHeight = Math.max(200, height + (position.y - e.clientY));
-            const newY = Math.min(position.y + height - 150, e.clientY) ;
-            setHeight(newHeight) ;
-            setPosition((prev) => ({...prev, y:newY})) ;
+        if(isDraggingCameraman){
+            setCamermanPosition((prev) => ({
+                x:e.clientX-50, 
+                //y:Math.min(0, Math.max(e.clientY - window.innerHeight, -300)),
+            })) ;
+        } else if (isDraggingTarget){
+            // const newHeight = Math.max(200, height + (position.y - e.clientY));
+            // const newY = Math.min(position.y + height - 150, e.clientY) ;
+            // setHeight(newHeight) ;
+            // setPosition((prev) => ({...prev, y:newY})) ;
+            setTargetPosition((prev) => ({
+                x: e.clientX -50,
+                y: Math.max(-300, Math.min(0, prev.y - (e.movementY))) ,
+            })) ;
         }
     };
 
     const handlePointerUp = () => {
-        setIsDragging(false);
-        setIsResizing(false);
+        setIsDraggingCameraman(false);
+        setIsDraggingTarget(false);
     }
 
     return(
@@ -43,23 +52,27 @@ export default function DraggableResizableShape() {
             <Canvas className="w-screen h-screen" onPointerMove={handlePointerMove} onPointerUp={handlePointerUp}>
                 <Html fullscreen>
                     <Navbar />
-                    <div>
-                        <Backcity className="absolute top-0 left-0 object-cover w-full h-full -z-10 opacity-65"/>
+
+                    <div className="absolute inset-0 w-full h-full -z-10">
+                        <Backcity className="object-cover w-full h-full opacity-65"/>
                     </div>
+
                     <div 
                         className="absolute cursor-move"
                         style={{
-                                    left: `${position.x}px`,
-                                    top: `${position.y}px`,
-                                    height: `${height}px`,
-                                    width : `${height/3}px`,
+                                    left: `${CameramanPosition.x}px`,
+                                    bottom: `${CameramanPosition.y}px`,
+                                    height: "300px",
+                                    width : "100px",
+                                    zIndex: 10,
                                 }}
-                        onPointerDown={handlePointerDownDrag}       
+                        onPointerDown={() => setIsDraggingCameraman(true)}       
                     >
-                            <Cameraman className="w-full h-full z-10 absolute bottom-0" 
+                            <Cameraman className="w-full h-full z-10 " 
                         /> 
                     </div>
-                    <div 
+
+                    {/* <div 
                         className="absolute cursor-ns-resize bg-blue-500"
                         style={{
                             left: `${position.x +(height/3)/2 -20}px`,
@@ -69,12 +82,22 @@ export default function DraggableResizableShape() {
                             zIndex:20
                         }}        
                         onPointerDown={handlePointerDownResize}
-                    />
-                    <div className="w-full h-full ">
-                        <div className="absolute w-1/6 h-full bottom-0 right-0">
-                            <Target className="w-full h-full z-10" />
-                        </div>
+                    /> */}
+
+                    <div 
+                        className="absolute cursor-move"
+                        style={{
+                            left: `${targetPosition.x}px`,
+                            bottom: `${targetPosition.y}px`,
+                            height: "400px",
+                            width: "200px",
+                            zIndex: 10,
+                        }}
+                        onPointerDown={()=> setIsDraggingTarget(true)}
+                    >
+                            <Target className="w-full h-full z-10" />   
                     </div>
+
                 </Html>
             </Canvas>
         </div>
